b52a0866b112c49503dbded9801107aafaa2c8ce
Nikhil Wadatkar <nikhil.wadatkar4693@gmail.com>
2022-04-01 05:49:42 +0530

    line added from local master

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println ("Ganesh branch 01");
+        System.out.println ("line added from local master");
     }
 }
